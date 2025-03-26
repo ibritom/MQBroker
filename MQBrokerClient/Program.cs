5b209b06c992ed554b60344d0b696825684fb040
PICADO PICADO JEISON JOHEL <jeijoel@estudiantec.cr>
2025-03-26 13:11:05 -0600

    Agrega los proyectos del MQBRokerClient, de las estructuras de datos y del la interfaz gráfica (App)

---
+++ MQBrokerClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MQBrokerClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
